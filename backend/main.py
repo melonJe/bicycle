625c5b7fb7d61448299a3ae5b23e7ffcde18aa99
i7-14700K <cabs0814@naver.com>
2025-05-16 18:52:47 +0900

    fix: Dockerfile 작성 및 docker 오류 수정

--- backend/main.py
+++ backend/main.py
@@ -6,3 +6,8 @@ app = FastAPI(title="My FastAPI App", version="0.1.0")
 
 # 라우터 등록
 app.include_router(paths_router, prefix="/paths", tags=["paths"])
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
